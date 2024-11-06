58568ea68867206d5cb712a2a88d69ed7f7ce2d1
iamBharatManral <justbharatmanral@gmail.com>
2024-11-06 10:13:31 +0530

    feat: complete get, update and delete post api

--- src/handlers/posts.ts
+++ src/handlers/posts.ts
@@ -7,23 +7,48 @@ export function createPost(req: Request, res: Response) {
   post.createdAt = new Date();
   post.updatedAt = new Date();
   req.store?.save(post);
-  res.status(201).send(req.body)
+  res.status(201).send({
+    data: [post],
+    error: null
+  })
 }
 
 export function updatePost(req: Request, res: Response) {
-  res.send("Updated Post");
+  const { id } = req.params;
+  const newPost = req.body;
+  if (!newPost) {
+    res.status(404).json({ error: "Not Found", data: [] });
+    return;
+  }
+  newPost.id = id;
+  newPost.updatedAt = new Date();
+  req.store?.update(id, newPost);
+  res.status(200).send({ error: null, data: [newPost] });
 }
 
 export function deletePost(req: Request, res: Response) {
-  res.send("Deleted Post");
+  const { id } = req.params;
+  const post = req.store?.get(id);
+  if (!post) {
+    res.status(404).json({ error: "Not Found", data: [] });
+    return;
+  }
+  req.store?.delete(id);
+  res.status(200).send({ error: null, data: [] });
 }
 
 export function getPost(req: Request, res: Response) {
-  res.send("Get Post");
+  const { id } = req.params;
+  const post = req.store?.get(id);
+  if (!post) {
+    res.status(404).json({ error: "Not Found", data: [] });
+    return;
+  }
+  res.status(200).json({ data: [post], error: null });
 }
 
 export function getAll(req: Request, res: Response) {
   res.status(200)
     .header('content-type', 'application/json')
-    .json(req.store?.getAll())
+    .json({ error: null, data: req.store?.getAll() })
 }
